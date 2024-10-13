fd275444f8f18a80cfd0995184155a76bab1b481
Skorodumov Michaile <m300881@mail.ru>
2024-10-13 14:10:32 +0300

    test db context is mongo

--- NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.IntegrationTests/TestWebApplicationFactory.cs
+++ NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.IntegrationTests/TestWebApplicationFactory.cs
@@ -3,10 +3,15 @@ using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Pcf.GivingToCustomer.Core.Abstractions.Gateways;
+using Pcf.GivingToCustomer.Core.Abstractions.Repositories;
 using Pcf.GivingToCustomer.DataAccess;
+using Pcf.GivingToCustomer.DataAccess.Repositories;
+using Pcf.GivingToCustomer.DataAccess.Data;
+using Pcf.GivingToCustomer.DataAccess.Infrastructure;
 using Pcf.GivingToCustomer.Integration;
 using Pcf.GivingToCustomer.IntegrationTests.Data;
 
@@ -23,35 +28,49 @@ namespace Pcf.GivingToCustomer.IntegrationTests
                     d => d.ServiceType ==
                          typeof(DbContextOptions<DataContext>));
 
+                var configuration = new ConfigurationBuilder()
+                    .AddEnvironmentVariables()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
                 services.Remove(descriptor);
 
+                services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
                 services.AddScoped<INotificationGateway, NotificationGateway>();
-                
-                services.AddDbContext<DataContext>(x =>
-                {
-                    x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
-                    //x.UseNpgsql(Configuration.GetConnectionString("PromoCodeFactoryDb"));
-                    x.UseSnakeCaseNamingConvention();
-                    x.UseLazyLoadingProxies();
-                });
+                services.AddScoped<IDbInitializer, EfTestDbInitializer>();
 
-                var sp = services.BuildServiceProvider();
+                services.Configure<MongoDBSettings>(configuration.GetSection(nameof(MongoDBSettings)));
 
-                using var scope = sp.CreateScope();
-                var scopedServices = scope.ServiceProvider;
-                var dbContext = scopedServices.GetRequiredService<DataContext>();
-                var logger = scopedServices
-                    .GetRequiredService<ILogger<TestWebApplicationFactory<TStartup>>>();
+                services.AddScoped<DbContext, MongoDBContext>();
+
+                services.AddDbContext<MongoDBContext>();
+
+                //services.AddDbContext<DataContext>(x =>
+                //{
+                //    x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
+                //    //x.UseNpgsql(Configuration.GetConnectionString("PromoCodeFactoryDb"));
+                //    x.UseSnakeCaseNamingConvention();
+                //    x.UseLazyLoadingProxies();
+                //});
+
+                //var sp = services.BuildServiceProvider();
+
+                //using var scope = sp.CreateScope();
+                //var scopedServices = scope.ServiceProvider;
+                ////var dbContext = scopedServices.GetRequiredService<DataContext>();
+                //var dbContext = scopedServices.GetRequiredService<MongoDBContext>();
+                //var logger = scopedServices
+                //    .GetRequiredService<ILogger<TestWebApplicationFactory<TStartup>>>();
                 
-                try
-                {
-                    new EfTestDbInitializer(dbContext).InitializeDb();
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Проблема во время заполнения тестовой базы. " +
-                                        "Ошибка: {Message}", ex.Message);
-                }
+                //try
+                //{
+                //    new EfTestDbInitializer(dbContext).InitializeDb();
+                //}
+                //catch (Exception ex)
+                //{
+                //    logger.LogError(ex, "Проблема во время заполнения тестовой базы. " +
+                //                        "Ошибка: {Message}", ex.Message);
+                //}
             });
         }
     }
